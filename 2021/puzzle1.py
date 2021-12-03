442fe14be1849c24db0e1a6841f30a8fa1ff469e
Russell Helmstedter <rhelmstedter@gmail.com>
2021-12-02 20:01:16 -0800

    updated 2021 day 2

---
+++ 2021/puzzle1.py
@@ -0,0 +1,51 @@
+test = """199 200 208 210 200 207 240 269 260 263""".split()
+
+with open("inputs/day1.txt", "r") as input:
+    sonar_sweeps = input.read().split("\n")
+
+
+def two_sliding_window(sweeps):
+    count = 0
+    for current_line, next_line in zip(sweeps, sweeps[1:]):
+        if int(current_line) < int(next_line):
+            count += 1
+    return count
+
+
+def three_measurement_zip(sweeps):
+    windows = []
+    for (
+        pane1,
+        pane2,
+        pane3,
+    ) in zip(sweeps, sweeps[1:], sweeps[2:]):
+        window = sum([int(pane1), int(pane2), int(pane3)])
+        windows.append(window)
+    return windows
+
+
+def three_sliding_window(sweeps):
+    count = 0
+    for i in range(len(sweeps) - 3):
+        window1 = sum(
+            [
+                int(sweeps[i]),
+                int(sweeps[i + 1]),
+                int(sweeps[i + 2]),
+            ]
+        )
+        window2 = sum(
+            [
+                int(sweeps[i + 1]),
+                int(sweeps[i + 2]),
+                int(sweeps[i + 3]),
+            ]
+        )
+        if window2 > window1:
+            count += 1
+    return count
+
+
+print(f"Part 1: {two_sliding_window(sonar_sweeps)}")
+print(f"Part 2: {three_sliding_window(sonar_sweeps)}")
+print(f"Part2 funcs: {two_sliding_window(three_measurement_zip(sonar_sweeps))}")
---
+++ 2021/puzzle2.py
@@ -0,0 +1,55 @@
+from pyperclip import copy
+
+with open("inputs/day2.txt", "r") as input:
+    course = input.read().split("\n")
+
+
+def final_destination_simple(course):
+    range = 0
+    depth = 0
+    parsed_course = [direction.split() for direction in course]
+
+    for bearing, board in parsed_course:
+        if bearing == "forward":
+            range += int(board)
+        elif bearing == "up":
+            depth -= int(board)
+        elif bearing == "down":
+            depth += int(board)
+
+    return range, depth
+
+
+def final_destination_with_aim(course):
+    range = 0
+    depth = 0
+    aim = 0
+    parsed_course = [direction.split() for direction in course]
+
+    for bearing, board in parsed_course:
+        if bearing == "forward":
+            range += int(board)
+            depth += aim * int(board)
+        elif bearing == "up":
+            aim -= int(board)
+        elif bearing == "down":
+            aim += int(board)
+
+    return range, depth
+
+
+def part_1():
+    range, depth = final_destination_simple(course)
+    print(range * depth)
+    copy(range * depth)
+
+
+def part_2():
+    range, depth = final_destination_with_aim(course)
+    print(range * depth)
+    copy(range * depth)
+
+
+if __name__ == "__main__":
+    # part_1()
+    part_2()
