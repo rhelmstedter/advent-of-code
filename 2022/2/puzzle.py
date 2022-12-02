2e01818145edc218f9f0749cec1be96783955c59
Russell Helmstedter <rhelmstedter@gmail.com>
2022-12-02 07:01:31 -0800

    day 2

---
+++ 2022/2/puzzle.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+day = 2
+
+with open(f"../inputs/{day}.txt") as input:
+    lines = [line.strip("\n") for line in input.readlines()]
+
+
+def part1(lines):
+    """total score from strategy"""
+    df = pd.read_csv("./rps.csv", index_col="my_move")
+    my_score = 0
+    for line in lines:
+        opponent_move, my_move = line.split()
+        if my_move == "X":
+            my_score += 1
+        elif my_move == "Y":
+            my_score += 2
+        elif my_move == "Z":
+            my_score += 3
+
+        if df.loc[my_move, opponent_move] == "win":
+            my_score += 6
+        elif df.loc[my_move, opponent_move] == "tie":
+            my_score += 3
+        elif df.loc[my_move, opponent_move] == "loss":
+            my_score += 0
+    return my_score
+
+
+def part2(lines):
+    """ """
+    df = pd.read_csv("./new_rules.csv", index_col="my_move")
+    my_score = 0
+    for line in lines:
+        opponent_move, my_move = line.split()
+        if my_move == "X":
+            my_score += 0
+        elif my_move == "Y":
+            my_score += 3
+        elif my_move == "Z":
+            my_score += 6
+
+        if df.loc[my_move, opponent_move] == "A":
+            my_score += 1
+        elif df.loc[my_move, opponent_move] == "B":
+            my_score += 2
+        elif df.loc[my_move, opponent_move] == "C":
+            my_score += 3
+    return my_score
+
+
+if __name__ == "__main__":
+    from pyperclip import copy
+
+    # copy(part1(lines))
+    copy(part2(lines))
--- 2022/puzzle_template.py
+++ 2022/puzzle_template.py
@@ -1,5 +1,21 @@
 day = ...
 
-with open(f'../inputs/{day}') as input:
-    lines = [line.strip('\n') for line in input.readlines()]
+with open(f"../inputs/{day}") as input:
+    lines = [line.strip("\n") for line in input.readlines()]
 
+
+def part1(lines):
+    """ """
+    ...
+
+
+def part2(lines):
+    """ """
+    ...
+
+
+if __name__ == "__main__":
+    from pyperclip import copy
+
+    copy(part1(lines))
+    # copy(part2(lines))
