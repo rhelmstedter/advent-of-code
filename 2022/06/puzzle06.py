1656ff13fd1b53fd667214ad831ef2932b770165
Russell Helmstedter <rhelmstedter@gmail.com>
2022-12-05 23:29:32 -0800

    day 6 template

---
+++ 2022/06/puzzle06.py
@@ -0,0 +1,24 @@
+from aocd import get_data, submit
+
+
+def get_lines(day: int) -> list:
+    """Uses aocd to get the input then splits it into lines."""
+    return get_data(day=day, year=2022).splitlines()
+
+
+def part1(lines):
+    """ """
+    ...
+
+
+def part2(lines):
+    """ """
+    ...
+
+
+if __name__ == "__main__":
+
+    day = 6
+    lines = get_lines(day)
+    # submit(part1(lines), part="a", day=day, year=2022)
+    # submit(part2(lines), part="b", day=day, year=2022)
