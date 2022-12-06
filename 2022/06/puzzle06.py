d9893a6f22f20dbc4219ce0af379e310e40bb456
Helmstedter <russell.helmstedter@venturausd.org>
2022-12-06 12:31:18 -0800

    solved puzzle 6

--- 2022/06/puzzle06.py
+++ 2022/06/puzzle06.py
@@ -1,24 +1,37 @@
 from aocd import get_data, submit
+from collections import deque
 
 
-def get_lines(day: int) -> list:
-    """Uses aocd to get the input then splits it into lines."""
-    return get_data(day=day, year=2022).splitlines()
+# def get_lines(day: int) -> list:
+#     """Uses aocd to get the input then splits it into lines."""
+#     return get_data(day=day, year=2022).splitlines()
+
+def _sliding_window(lines, window_size):
+    window = deque(lines[:window_size], maxlen=window_size)
+    for chars_processed, c in enumerate(lines[window_size:], window_size):
+        if len(set(window)) == len(window):
+            break
+        else:
+            window.append(c)
+    return chars_processed
 
 
 def part1(lines):
     """ """
-    ...
+    return _sliding_window(lines, 4)
 
 
 def part2(lines):
     """ """
-    ...
+    return _sliding_window(lines, 14)
 
 
 if __name__ == "__main__":
 
     day = 6
-    lines = get_lines(day)
+    # lines = get_lines(day)
+    with open("./input.txt", "r") as input_file:
+        lines = input_file.read()
+    print(part2(lines))
     # submit(part1(lines), part="a", day=day, year=2022)
     # submit(part2(lines), part="b", day=day, year=2022)
--- 2022/06/test_puzzle06.py
+++ 2022/06/test_puzzle06.py
@@ -1,17 +1,16 @@
 from puzzle06 import part1, part2
 
-SAMPLE_INPUT = """
-"""
+SAMPLE_INPUT = """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
 EXAMPLE_LINES = [line.strip("\n") for line in SAMPLE_INPUT.split()]
-PART1_EXPECTED = ...
-PART2_EXPECTED = ...
+PART1_EXPECTED = 7
+PART2_EXPECTED = 19
 
 
 def test_part1():
-    actual = part1(EXAMPLE_LINES)
+    actual = part1(SAMPLE_INPUT)
     assert actual == PART1_EXPECTED
 
 
 def test_part2():
-    actual = part2(EXAMPLE_LINES)
+    actual = part2(SAMPLE_INPUT)
     assert actual == PART2_EXPECTED
