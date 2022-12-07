b2c20b1b392fce80066e538436714fb93b67ea35
Russell Helmstedter <rhelmstedter@gmail.com>
2022-12-06 17:56:13 -0800

    refactored day 6

--- 2022/06/puzzle06.py
+++ 2022/06/puzzle06.py
@@ -2,13 +2,17 @@ from aocd import get_data, submit
 from collections import deque
 
 
-# def get_lines(day: int) -> list:
-#     """Uses aocd to get the input then splits it into lines."""
-#     return get_data(day=day, year=2022).splitlines()
+def get_lines(day: int) -> list:
+    """Uses aocd to get the input then splits it into data."""
+    return get_data(day=day, year=2022).splitlines()
 
-def _sliding_window(lines, window_size):
-    window = deque(lines[:window_size], maxlen=window_size)
-    for chars_processed, c in enumerate(lines[window_size:], window_size):
+
+def _sliding_window(data, window_size):
+    """Return the number of characters processed until a window with all
+    unique chars.
+    """
+    window = deque(data[:window_size], maxlen=window_size)
+    for chars_processed, c in enumerate(data[window_size:], window_size):
         if len(set(window)) == len(window):
             break
         else:
@@ -16,22 +20,19 @@ def _sliding_window(lines, window_size):
     return chars_processed
 
 
-def part1(lines):
-    """ """
-    return _sliding_window(lines, 4)
+def part1(data):
+    """Use a sliding window of width 4."""
+    return _sliding_window(data, 4)
 
 
-def part2(lines):
-    """ """
-    return _sliding_window(lines, 14)
+def part2(data):
+    """Use a sliding window of width 14."""
+    return _sliding_window(data, 14)
 
 
 if __name__ == "__main__":
 
     day = 6
-    # lines = get_lines(day)
-    with open("./input.txt", "r") as input_file:
-        lines = input_file.read()
-    print(part2(lines))
-    # submit(part1(lines), part="a", day=day, year=2022)
-    # submit(part2(lines), part="b", day=day, year=2022)
+    data = get_data(day=day, year=2022)
+    # submit(part1(data), part="a", day=day, year=2022)
+    # submit(part2(data), part="b", day=day, year=2022)
