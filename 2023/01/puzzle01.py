4862a451eda0919503938b3bed9b8c8483781c67
Russell Helmstedter <rhelmstedter@gmail.com>
2023-12-02 10:01:26 -0800

    solved 2023 day 01

---
+++ 2023/01/puzzle01.py
@@ -0,0 +1,64 @@
+import aocd
+import regex as re
+
+
+WORDS_TO_DIGITS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+def get_data(day: int, lines: bool = True) -> str | list:
+    """Uses aocd to get the input then splits it into lines."""
+    if lines:
+        return aocd.get_data(day=day, year=2023).splitlines()
+    else:
+        return aocd.get_data(day=day, year=2023)
+
+
+def part1(data):
+    """ """
+    digits = list("0123456789")
+    filter = ["".join(c for c in line if c in digits) for line in data if line]
+    return sum(int(cal[0] + cal[-1]) for cal in filter)
+
+
+def part2(data):
+    """ """
+    pattern = r"""
+    \d
+    | one
+    | two
+    | three
+    | four
+    | five
+    | six
+    | seven
+    | eight
+    | nine
+    """
+    calibrations = []
+    for line in data:
+        digits = re.findall(pattern, line, overlapped=True, flags=re.VERBOSE)
+        converted = []
+        for d in digits:
+            if d in WORDS_TO_DIGITS:
+                d = WORDS_TO_DIGITS[d]
+            converted.append(d)
+        calibrations.append(int(converted[0] + converted[-1]))
+    return sum(calibrations)
+
+
+if __name__ == "__main__":
+    day = 1
+    data = get_data(day, True)
+    part2(data)
+    # aocd.submit(part1(data), part="a", day=day, year=2023)
+    aocd.submit(part2(data), part="b", day=day, year=2023)
---
+++ 2023/01/test_puzzle01.py
@@ -0,0 +1,29 @@
+from puzzle01 import part1, part2
+
+
+PART1_INPUT = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+
+SAMPLE_INPUT = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+PART1_EXPECTED = 142
+PART2_EXPECTED = 281
+
+
+def test_part1():
+    actual = part1(PART1_INPUT.splitlines())
+    assert actual == PART1_EXPECTED
+
+
+def test_part2():
+    actual = part2(SAMPLE_INPUT.splitlines())
+    assert actual == PART2_EXPECTED
