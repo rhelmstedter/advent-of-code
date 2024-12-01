0541f6f536899a0ff1b7144d13b381059764e0c5
Russell Helmstedter <rhelmstedter@gmail.com>
2024-12-01 13:14:58 -0800

    made 2024 template

---
+++ 2024/2024-template/test_puzzle.py
@@ -0,0 +1,18 @@
+from puzzle import part1, part2
+
+SAMPLE_INPUT = """
+"""
+
+
+PART1_EXPECTED = ...
+PART2_EXPECTED = ...
+
+
+def test_part1():
+    actual = part1(SAMPLE_INPUT.splitlines())
+    assert actual == PART1_EXPECTED
+
+
+def test_part2():
+    actual = part2(SAMPLE_INPUT.splitlines())
+    assert actual == PART2_EXPECTED
---
+++ 2024/template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "What day are you starting? "
+read DAY
+
+mkdir $DAY
+cp ./2024-template/*.py ./$DAY
+
+mv ./$DAY/puzzle.py ./$DAY/puzzle$DAY.py
+mv ./$DAY/test_puzzle.py ./$DAY/test_puzzle$DAY.py
+
+if [[ ${DAY:0:1} == 0 ]]; then
+    sed -i "" "s/day = .../day = ${DAY:1}/g" ./$DAY/puzzle$DAY.py 
+else
+    sed -i "" "s/day = .../day = $DAY/g" ./$DAY/puzzle$DAY.py 
+fi
+
+
+sed -i "" "s/from puzzle/from puzzle$DAY/" ./$DAY/test_puzzle$DAY.py
