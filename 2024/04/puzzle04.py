6b1ffe19072a3399c2d638ca93d2b3974edff836
Russell Helmstedter <rhelmstedter@gmail.com>
2024-12-03 22:20:35 -0800

    initialized day 4; too late to start

---
+++ 2024/04/puzzle04.py
@@ -0,0 +1,29 @@
+import aocd
+
+
+def get_data(day: int, lines: bool = True) -> str | list:
+    """Uses aocd to get the input then splits it into lines."""
+    if lines:
+        return aocd.get_data(day=day, year=2024).splitlines()
+    else:
+        return aocd.get_data(day=day, year=2024)
+
+
+def part1(data):
+    """ """
+    ...
+
+
+
+
+def part2(data):
+    """ """
+    ...
+
+
+if __name__ == "__main__":
+
+    day = 4
+    data = get_data(day)
+    # aocd.submit(part1(data), part="a", day=day, year=2024)
+    # aocd.submit(part2(data), part="b", day=day, year=2024)
---
+++ 2024/04/test_puzzle04.py
@@ -0,0 +1,27 @@
+from puzzle04 import part1, part2
+
+SAMPLE_INPUT = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+"""
+
+
+PART1_EXPECTED = 18
+PART2_EXPECTED = ...
+
+
+def test_part1():
+    actual = part1(SAMPLE_INPUT.splitlines())
+    assert actual == PART1_EXPECTED
+
+
+def test_part2():
+    actual = part2(SAMPLE_INPUT.splitlines())
+    assert actual == PART2_EXPECTED
