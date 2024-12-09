5e847b01811d92d59c0d0d8b77e223d5cb0fe6c5
Russell Helmstedter <rhelmstedter@gmail.com>
2024-12-08 19:58:44 -0800

    refactored

--- 2024/05/puzzle05.py
+++ 2024/05/puzzle05.py
@@ -10,23 +10,49 @@ def get_data(day: int, lines: bool = True) -> str | list:
         return aocd.get_data(day=day, year=2024)
 
 
-def part1(data):
-    """ """
-    orders, reports = data.split("\n\n")
+def build_order_stacks(orders) -> None:
     order_stacks = defaultdict(list)
     for line in orders.splitlines():
         page, rule = map(int, line.split("|"))
         order_stacks[page].append(rule)
+    return order_stacks, page
+
+
+def fix_bad_lines(bad, line, order_stacks, page) -> None:
+    for i, page in enumerate(line):
+        pages_in_rule = set(line) & set(order_stacks[page])
+        if pages_in_rule and not all(
+            line.index(page) < line.index(p) for p in pages_in_rule
+        ):
+            bad = True
+            line = sorted(
+                line,
+                key=lambda x: len(line) if x in order_stacks[page] else line.index(x),
+            )
+    return bad, line
+
+
+def identify_bad_line(bad, line, order_stacks, page) -> None:
+    for i, page in enumerate(line):
+        pages_in_rule = set(line) & set(order_stacks[page])
+        if pages_in_rule and not all(
+            line.index(page) < line.index(p) for p in pages_in_rule
+        ):
+            bad = True
+            break
+    return bad
+
+
+def part1(data):
+    """ """
+    orders, reports = data.split("\n\n")
+    order_stacks, page = build_order_stacks(orders)
+
     good = []
     for line in reports.splitlines():
         bad = False
         line = [int(x) for x in line.split(",")]
-        for i, page in enumerate(line):
-            pages_in_rule = set(line) & set(order_stacks[page])
-            if pages_in_rule:
-                if not all(line.index(page) < line.index(p) for p in pages_in_rule):
-                    bad = True
-                    break
+        bad = identify_bad_line(bad, line, order_stacks, page)
         if not bad:
             good.append(line[len(line) // 2])
     return sum(good)
@@ -35,21 +61,13 @@ def part1(data):
 def part2(data):
     """ """
     orders, reports = data.split("\n\n")
-    order_stacks = defaultdict(list)
-    for line in orders.splitlines():
-        page, rule = map(int, line.split("|"))
-        order_stacks[page].append(rule)
+    order_stacks, page = build_order_stacks(orders)
 
     good = []
     for line in reports.splitlines():
         bad = False
         line = [int(x) for x in line.split(",")]
-        for i, page in enumerate(line):
-            pages_in_rule = set(line) & set(order_stacks[page])
-            if pages_in_rule:
-                if not all(line.index(page) < line.index(p) for p in pages_in_rule):
-                    bad = True
-                    line = sorted(line, key=lambda x: len(line) if x in order_stacks[page] else line.index(x))
+        bad, line = fix_bad_lines(bad, line, order_stacks, page)
         if bad:
             good.append(line[len(line) // 2])
     return sum(good)
