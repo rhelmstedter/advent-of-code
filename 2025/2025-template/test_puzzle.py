5eab1c86bbd54bec9494fe28f85edabc0cd7a399
Russell Helmstedter <rhelmstedter@gmail.com>
2025-12-01 22:19:44 -0800

    solved part 1; stuck on part 2

--- 2025/2025-template/test_puzzle.py
+++ 2025/2025-template/test_puzzle.py
@@ -1,11 +1,20 @@
 from puzzle import part1, part2
 
-SAMPLE_INPUT = """
+SAMPLE_INPUT = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
 """
 
 
-PART1_EXPECTED = ...
-PART2_EXPECTED = ...
+PART1_EXPECTED = 3
+PART2_EXPECTED = 6
 
 
 def test_part1():
@@ -16,3 +25,7 @@ def test_part1():
 def test_part2():
     actual = part2(SAMPLE_INPUT.splitlines())
     assert actual == PART2_EXPECTED
+
+
+if __name__ == "__main__":
+    print(part2(SAMPLE_INPUT.splitlines()))
