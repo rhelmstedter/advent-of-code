4c649797830606edbd6bc310bb747f805aa6ca2d
Russell Helmstedter <rhelmstedter@gmail.com>
2025-12-04 14:28:18 -0800

    solved day 3 part 2

--- 2025/03/puzzle03.py
+++ 2025/03/puzzle03.py
@@ -10,24 +10,34 @@ def get_data(day: int, lines: bool = True) -> str | list:
 
 
 def part1(data):
-    """ """
+    """This works because we are finding the largest value in the list of integers as long
+    as its not in the final position. Then, using list.index(), we find us the first occurance
+     the max, and only search from that point on."""
     total = 0
-    for bank in data:
-        max_value = -1
-        for i in range(len(bank)):
-            first = bank[i]
-            for j in range(i + 1, len(bank)):
-                second = bank[j]
-                current_value = int(first + second)
-                if current_value > max_value:
-                    max_value = current_value
-        total += max_value
+    banks = [[int(b) for b in line] for line in data]
+    for bank in banks:
+        tens = max(bank[:-1])
+        ones = max(bank[bank.index(tens) + 1 :])
+        total += tens * 10 + ones
     return total
 
 
 def part2(data):
-    """ """
-    ...
+    """This leverages the same logic as the first, but instead of finding two digits, we iterate
+    up to 11 digits. Then we take care of the 12th outside of the loop. If we iterated all 12
+    inside the loop, we would end up with list[:0] which is the empty list."""
+    total = 0
+    banks = [[int(b) for b in line] for line in data]
+    for bank in banks:
+        jolts = 0
+        for i in range(11):
+            digit = max(bank[:i-11])
+            bank = bank[bank.index(digit) + 1 :]
+            jolts = jolts * 10 + digit
+        jolts = jolts * 10 + max(bank)
+        print(jolts)
+        total += jolts
+    return total
 
 
 if __name__ == "__main__":
